4e15cd1b3ee5cdbeb6a62b8e9994cdc96a9c4deb
Fabio Ayres <fabioja@insper.edu.br>
2022-09-02 09:00:34 -0300

    wip: refactoring init

---
+++ setup.py
@@ -0,0 +1,9 @@
+# pylint: disable=missing-docstring
+from setuptools import setup, find_packages
+
+setup(
+    name='diver',
+    version='0.1',
+    packages=find_packages(),
+    description='DIVeR: Deterministic Integration for Volume Rendering',
+)
